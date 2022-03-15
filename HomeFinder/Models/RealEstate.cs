ea82ef858ae2f88d18938c68029bb986efc460e5
Madeleine <madeleine.vagero@gmail.com>
2022-03-15 09:16:39 +0100

    added page for objects

--- HomeFinder/Models/RealEstate.cs
+++ HomeFinder/Models/RealEstate.cs
@@ -7,14 +7,24 @@ namespace HomeFinder.Models
     public class RealEstate
     {
         public int Id { get; set; }
+        [Required]
         public string Address { get; set; }
         public string Pictures { get; set; }
         public string Description { get; set; }
+        [Display(Name = "Form Of Lease")]
+        [Required]
         public string FormOfLease { get; set; }
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+        [Display(Name = "Rooms")]
         public int NumberOfRooms { get; set; }
+        [Display(Name = "Living Area")]
         public int LivingArea { get; set; }
+        [Display(Name = "Construction Year")]
+        [DataType(DataType.Date)]
         public DateTime ConstructionYear { get; set; }
+        [Display(Name = "Show Date")]
+        [DataType(DataType.Date)]
         public DateTime ShowDate { get; set; }
         public List<RegistrationOfInterest> RegistrationsOfInterest{ get; set; }
     }
