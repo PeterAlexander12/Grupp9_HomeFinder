11d510335e73b18891b818029133936f398c621f
Peter L <peter.lindholm@iths.se>
2022-03-26 15:41:26 +0100

    Add Annotations

--- HomeFinder/Models/ApplicationUser.cs
+++ HomeFinder/Models/ApplicationUser.cs
@@ -12,26 +12,25 @@ namespace HomeFinder.Models
         //[Display(Name ="Användarnamn")]
         //public string? UserName { get; set; }
 
-        [Required]
-        [Display(Name = "Förnamn")]
+        //[Required]
+        //[Display(Name = "Förnamn")]
         public string GivenName { get; set; }
-        [Required]
-        [Display(Name = "Efternamn")]
+        //[Required]
+        //[Display(Name = "Efternamn")]
         public string SurName { get; set; }
 
         //[Phone]
         //[Display(Name = "Telefonnummer")]
         //public string? PhoneNumber { get; set; }
 
-        [Required]
-        [Display(Name = "Lösenord")]
-        [DataType(DataType.Password)]
+        //[Required]
+        //[Display(Name = "Lösenord")]
+        //[DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Bekräfta lösenord")]
-        [Compare("Password",
-            ErrorMessage = "Your password does not match,")]
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Bekräfta lösenord")]
+        //[Compare("Password", ErrorMessage = "Your password does not match,")]
         public string ConfirmPassword { get; set; }
 
     }
--- HomeFinder/Models/RegisterViewModel.cs
+++ HomeFinder/Models/RegisterViewModel.cs
@@ -1,13 +1,35 @@
-﻿namespace HomeFinder.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeFinder.Models
 {
     public class RegisterViewModel
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Display(Name = "Användarnamn")]
         public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Förnamn")]
         public string GivenName { get; set; }
+
+        [Required]
+        [Display(Name = "Efternamn")]
         public string SurName { get; set; }
+
+        [Phone]
+        [Display(Name = "Telefonnummer")]
         public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Lösenord")]
         public string Password  { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Bekräfta lösenord")]
+        [Compare("Password", ErrorMessage = "Det matchar inte ditt pucko")]
         public string ConfirmPassword { get; set; } 
     }
 }
