beed52048994d3d31136fd23f438d38894356f3b
abergjr <90029910+abergjr@users.noreply.github.com>
2022-04-06 13:22:35 +0200

    Changed layout for gallery page

--- HomeFinder/Controllers/GalleryController.cs
+++ HomeFinder/Controllers/GalleryController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using HomeFinder.Models;
 
 namespace HomeFinder.Controllers
 {
@@ -15,10 +16,14 @@ namespace HomeFinder.Controllers
         {
             _context = context; 
         }
-        public async Task<IActionResult> Index(string searchTerm, string maxSlide, string minSlide)
+        public async Task<IActionResult> Index(string searchTerm, string maxSlide, string minSlide, List<int> realEstateType, string removeFilter)
         {
             var realEstates = _context.RealEstate.Select(r => r);
 
+            if (!string.IsNullOrEmpty(removeFilter) && removeFilter.Equals("Rensa filter"))
+            {
+                return View(await realEstates.ToListAsync());
+            }
 
             if (!string.IsNullOrEmpty(searchTerm))
             {
@@ -27,19 +32,46 @@ namespace HomeFinder.Controllers
             if (!string.IsNullOrEmpty(maxSlide))
             {
                 int maxPrice = int.Parse(maxSlide);
+                if(maxPrice != 0)
+                {
                 realEstates = realEstates.Where(r => r.Price <= maxPrice);
+                }
             }
             if (!string.IsNullOrEmpty(minSlide))
             {
                 int minPrice = int.Parse(minSlide);
+
+                if(minPrice != 0)
+                {
                 realEstates = realEstates.Where(r => r.Price >= minPrice);
+                }
+            }
+            if(realEstateType.Count > 0)
+            {
+
+                if (realEstateType.Count == 1)
+                {
+                    realEstates = realEstates.Where(r => (int)r.RealEstateType == realEstateType.ElementAt(0));
+                }
+
+                if(realEstateType.Count == 2)
+                {
+                    realEstates = realEstates.Where(r => (int)r.RealEstateType == realEstateType.ElementAt(0) || (int)r.RealEstateType == realEstateType.ElementAt(1));
+                }
+
+                if (realEstateType.Count == 3)
+                {
+                    realEstates = realEstates.Where(r => (int)r.RealEstateType == realEstateType.ElementAt(0) 
+                    || (int)r.RealEstateType == realEstateType.ElementAt(1)
+                    || (int)r.RealEstateType == realEstateType.ElementAt(2));
+                }
             }
 
             return View(await realEstates.ToListAsync());
         }
 
         public async Task<IActionResult> Details(int? id)
-        {
+        {   
             if (id == null)
             {
                 return NotFound();
