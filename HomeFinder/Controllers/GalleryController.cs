9318c7ded91876e7be7ef11c4b50eb25d74beccb
LindholmFredrik <fredde.9_98@hotmail.com>
2022-04-08 12:17:00 +0200

    Merge layout - sök

--- HomeFinder/Controllers/GalleryController.cs
+++ HomeFinder/Controllers/GalleryController.cs
@@ -86,6 +86,53 @@ namespace HomeFinder.Controllers
 
             return View(realEstate);
         }
+        public async Task<IActionResult> AdvSearch(string searchTerm, string maxSlide, string minSlide, string minLivingSlide, string RealEstateType, string NumberOfRooms, string Ålderpåhus)
+        {
+            var realEstates = _context.RealEstate.Select(r => r);
 
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                realEstates = realEstates.Where(r => r.Address.Contains(searchTerm) || r.Description.Contains(searchTerm));
+            }
+            if (!string.IsNullOrEmpty(maxSlide))
+            {
+                int maxPrice = int.Parse(maxSlide);
+                realEstates = realEstates.Where(r => r.Price <= maxPrice);
+            }
+            if (!string.IsNullOrEmpty(minSlide))
+            {
+                int minPrice = int.Parse(minSlide);
+                realEstates = realEstates.Where(r => r.Price >= minPrice);
+            }
+            if (!string.IsNullOrEmpty(minLivingSlide))
+            {
+                int minLivingArea = int.Parse(minLivingSlide);
+                realEstates = realEstates.Where(r => r.LivingArea >= minLivingArea);
+            }
+            if (!string.IsNullOrEmpty(NumberOfRooms))
+            {
+                int numbOfRooms = int.Parse(NumberOfRooms);
+                realEstates = realEstates.Where(r => r.NumberOfRooms >= numbOfRooms);
+            }
+
+            if (!string.IsNullOrEmpty(RealEstateType))
+            {
+                RealEstateTypes test = (RealEstateTypes)Enum.Parse(typeof(RealEstateTypes), RealEstateType);
+                realEstates = realEstates.Where(r => r.RealEstateType.Equals(test));
+            }
+
+            if (!string.IsNullOrEmpty(Ålderpåhus))
+            {
+                if (Ålderpåhus != "Ingen")
+                {
+                    int _Ålderpåhus = int.Parse(Ålderpåhus);
+
+                    int Condition = DateTime.Now.Year - _Ålderpåhus;
+                    realEstates = realEstates.Where(r => r.ConstructionYear.Year >= Condition);
+                }
+            }
+
+            return View(await realEstates.ToListAsync());
+        }
     }
 }
