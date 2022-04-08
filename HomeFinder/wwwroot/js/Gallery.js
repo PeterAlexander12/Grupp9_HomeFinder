b851fe1af65f4d75b4ef1e9bdbe0ee0d5a158350
abergjr <90029910+abergjr@users.noreply.github.com>
2022-04-08 13:22:11 +0200

    Added JS to separate files

---
+++ HomeFinder/wwwroot/js/Gallery.js
@@ -0,0 +1,26 @@
+﻿function toggleClick(click) {
+
+    var button;
+    var checkbox;
+
+    if (click == 0) {
+        button = document.getElementById('house-button');
+        checkbox = document.getElementById("house");
+    }
+
+    if (click == 1) {
+        button = document.getElementById('apartment-button');
+        checkbox = document.getElementById("apartment");
+    }
+
+    if (click == 2) {
+        button = document.getElementById('farm-button');
+        checkbox = document.getElementById("farm");
+    }
+
+    if (checkbox.checked) {
+        button.style.backgroundColor = 'gray';
+    } else {
+        button.style.backgroundColor = 'white';
+    }
+}
