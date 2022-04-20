fa2c006341faa394191d404499a85692346d2ed1
Gitimon <persson.a.simon@gmail.com>
2022-04-20 22:38:28 +0200

    Added City for Real estates

---
+++ HomeFinder/Migrations/20220420192739_addedCityPropToRealEstate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeFinder.Migrations
+{
+    public partial class addedCityPropToRealEstate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "RealEstate",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "RealEstate");
+        }
+    }
+}
