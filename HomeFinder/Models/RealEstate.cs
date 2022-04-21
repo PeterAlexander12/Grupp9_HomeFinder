a1a399f360b41981ab2aec02ec387f98a7c24f2f
Madeleine <madeleine.vagero@gmail.com>
2022-04-21 11:06:51 +0200

    Updated RealEstate table

--- HomeFinder/Models/RealEstate.cs
+++ HomeFinder/Models/RealEstate.cs
@@ -12,32 +12,39 @@ namespace HomeFinder.Models
         [Required]
         [Display(Name = "Adress")]
         public string Address { get; set; }
+        [Display(Name = "Ort")]
+        public string City { get; set; }
+        [Display(Name = "URL för omslagsbild")]
         public string CoverPictureURL { get; set; }
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
 
         [DataType(DataType.Currency)]
         [Display(Name = "Utgångspris")]
         public int Price { get; set; }
 
-        [Display(Name = "Rooms")]
+        [Display(Name = "Antal rum")]
         public int NumberOfRooms { get; set; }
-        [Display(Name = "Living Area")]
+        [Display(Name = "Boarea")]
         public int LivingArea { get; set; }
+        [Display(Name = "Biarea")]
+        public int? SubsidiaryArea { get; set; }
+        [Display(Name = "Tomtarea")]
+        public int? LotArea { get; set; }
+    
         [Display(Name = "Byggår")]
         [DataType(DataType.Date)]
         public DateTime ConstructionYear { get; set; }
-        [Display(Name = "Visningsdatum")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Visningstid")]
         public DateTime ShowDate { get; set; }
 
-        public List<RegistrationOfInterest> RegistrationsOfInterest{ get; set; }
-
-        [Display(Name = "Upplåtelseform")]
-
+        [Display(Name = "Bostadstyp")]
         public RealEstateTypes RealEstateType { get; set; }
+        [Display(Name = "Upplåtelseform")]
+        public FormOfLease FormOfLease { get; set; } 
         public ICollection<RealEstateImage> RealEstateImages { get; set; }
         public IList<Favourite> Favourites { get; set; }
-
+        public List<RegistrationOfInterest> RegistrationsOfInterest{ get; set; }
 
     }
 }
