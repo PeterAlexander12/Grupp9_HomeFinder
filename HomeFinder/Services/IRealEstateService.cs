b45f9453f0f3c35c55ca331bab7dcec253579da2
Peter L <peter.lindholm@iths.se>
2022-05-04 11:25:53 +0200

    Add Services to HomeFinder

---
+++ HomeFinder/Services/IRealEstateService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HomeFinder.ViewModels;
+
+namespace HomeFinder.Services
+{
+    public interface IRealEstateService
+    {
+        Task<IEnumerable<RealEstateVm>> GetRealEstates();
+        Task<RealEstateVm> GetRealEstate(int id);
+        Task UpdateRealEstate(RealEstateVm updatedRealEstate);
+        Task AddRealEstate(RealEstateVm realEstate);
+    }
+}
---
+++ HomeFinder/Services/RealEstateService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using HomeFinder.ViewModels;
+
+namespace HomeFinder.Services
+{
+    public class RealEstateService : IRealEstateService
+    {
+        readonly HttpClient _httpClient;
+
+        public RealEstateService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<RealEstateVm>> GetRealEstates()
+        {
+            return await _httpClient.GetFromJsonAsync<RealEstateVm[]>("/api/RealEstate");
+        }
+
+        public async Task<RealEstateVm> GetRealEstate(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<RealEstateVm>($"/api/RealEstate/{id}");
+
+        }
+
+        public async Task UpdateRealEstate(RealEstateVm updatedRealEstate)
+        {
+            var response = await _httpClient.PutAsJsonAsync("https://localhost:44387/api/RealEstate", updatedRealEstate);
+            response.EnsureSuccessStatusCode();
+        }
+        
+        public async Task AddRealEstate(RealEstateVm realEstate)
+        {
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:44387/api/RealEstate", realEstate);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
