a2316ed1b9df528628d8efc55ade448b0f6d9691
Peter L <peter.lindholm@iths.se>
2022-05-07 11:56:36 +0200

    Repository update

--- API/Data/RealEstateRepository.cs
+++ API/Data/RealEstateRepository.cs
@@ -2,78 +2,80 @@
 using System.Threading.Tasks;
 using API.Data;
 using API.ViewModels;
+using HomeFinder.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class RealEstateRepository : IRealEstateRepository
     {
+        readonly RealEstateContext _context;
 
-        public RealEstateRepository()
+        public RealEstateRepository(RealEstateContext context)
         {
             _context = context;
         }
 
-        public async Task<IEnumerable<RealEstateVm>> GetRealEstates()
-        {
-            // use automapper
+        //public async Task<IEnumerable<RealEstateVm>> GetRealEstates()
+        //{
+        //    // use automapper
 
 
-            return await _context.RealEstate.ToListAsync();
+        //    return await _context.RealEstate.ToListAsync();
 
-        }
+        //}
 
-        public async Task<RealEstate> GetRealEstate(int id)
-        {
-            return await _context.RealEstate.Include(r => r.RegistrationsOfInterest).FirstOrDefaultAsync(r => r.Id == id );
-        }
+        //public async Task<RealEstate> GetRealEstate(int id)
+        //{
+        //    return await _context.RealEstate.Include(r => r.RegistrationsOfInterest).FirstOrDefaultAsync(r => r.Id == id );
+        //}
 
-        public async Task AddRealEstate(PostRealEstateVm model)
-        {
-            var result = await _context.RealEstate.AddAsync(model);
-            await _context.SaveChangesAsync();
-            return result.Entity;
-        }
+        //public async Task AddRealEstate(PostRealEstateVm model)
+        //{
+        //    var result = await _context.RealEstate.AddAsync(model);
+        //    await _context.SaveChangesAsync();
+        //    return result.Entity;
+        //}
 
-        public async Task<RealEstate> UpdateRealEstate(RealEstate realEstate)
-        {
-            var result = await _context.RealEstate.FirstOrDefaultAsync(r => r.Id == realEstate.Id);
+        //public async Task<RealEstate> UpdateRealEstate(RealEstate realEstate)
+        //{
+        //    var result = await _context.RealEstate.FirstOrDefaultAsync(r => r.Id == realEstate.Id);
 
-            if (result != null)
-            {
-                result.Address = realEstate.Address;
-                result.City = realEstate.City;
-                result.ConstructionYear = realEstate.ConstructionYear;
-                result.CoverPictureURL = realEstate.CoverPictureURL;
-                result.Description = realEstate.Description;
-                result.LivingArea = realEstate.LivingArea;
-                result.NumberOfRooms = realEstate.NumberOfRooms;
-                result.Price = realEstate.Price;
-                result.Address = realEstate.Address;
-                result.ConstructionYear = realEstate.ConstructionYear;
+        //    if (result != null)
+        //    {
+        //        result.Address = realEstate.Address;
+        //        result.City = realEstate.City;
+        //        result.ConstructionYear = realEstate.ConstructionYear;
+        //        result.CoverPictureURL = realEstate.CoverPictureURL;
+        //        result.Description = realEstate.Description;
+        //        result.LivingArea = realEstate.LivingArea;
+        //        result.NumberOfRooms = realEstate.NumberOfRooms;
+        //        result.Price = realEstate.Price;
+        //        result.Address = realEstate.Address;
+        //        result.ConstructionYear = realEstate.ConstructionYear;
 
-                result.Favourites = realEstate.Favourites;
-                result.RealEstateImages = realEstate.RealEstateImages;
+        //        result.Favourites = realEstate.Favourites;
+        //        result.RealEstateImages = realEstate.RealEstateImages;
 
-                await _context.SaveChangesAsync();
+        //        await _context.SaveChangesAsync();
 
-                return result;
-            }
+        //        return result;
+        //    }
 
-            return null;
-        }
+        //    return null;
+        //}
 
-        public async Task DeleteRealEstateAsync(int id)
-        {
-            var result = await _context.RealEstate.FirstOrDefaultAsync(r => r.Id == id);
-            if (result != null)
-            {
-                _context.RealEstate.Remove(result);
-                await _context.SaveChangesAsync();
-                return result;
-            }
+        //public async Task DeleteRealEstateAsync(int id)
+        //{
+        //    var result = await _context.RealEstate.FirstOrDefaultAsync(r => r.Id == id);
+        //    if (result != null)
+        //    {
+        //        _context.RealEstate.Remove(result);
+        //        await _context.SaveChangesAsync();
+        //        return result;
+        //    }
 
-            return null;
-        }
+        //    return null;
+        //}
     }
 }
